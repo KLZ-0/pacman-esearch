741269459347ec92d4232e265368d36d50d80a76
KLZ-0 <adrian@kalazi.com>
2020-08-23 16:50:04 +0200

    Error handling in util.c

--- src/util.c
+++ src/util.c
@@ -2,20 +2,26 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <pwd.h>
 
 /**
  * @brief Appends the given relpath to the home directory path
  * @param relpath relative path "~/{relpath}"
- * @return Allocated char* with the full path, it needs to be freed afterwards
+ * @return Allocated char* with the full path or NULL on error, it needs to be freed afterwards
  */
 char* getHomePath(const char* relpath) {
 	struct passwd *pw = getpwuid(getuid());
+	if (pw == NULL)
+		return NULL;
+
 	const char *homedir = pw->pw_dir;
+	if (homedir == NULL)
+		return NULL;
 
 	char* fullpath = malloc(strlen(homedir) + strlen(relpath) + 1);
+	if (fullpath == NULL)
+		return NULL;
 	strcpy(fullpath, homedir);
 	strcat(fullpath, relpath);
 
